05866aaa5973bf1b37c5db5111d63ed52a88b545
Atakan Buyukoglu <buyukoglu.atakan@gmail.com>
2024-06-02 20:42:45 +0300

    Updates company infos on every run for record all

--- record_all.py
+++ record_all.py
@@ -10,18 +10,18 @@ records_path = Path(__file__).parent / "Data" / 'Valuation'
 records_path.mkdir(parents=True, exist_ok=True)
 
 kap = KAP(data_path=data_path)
-company_info = kap.get_company_info()
+company_info = kap.save_company_info()
 companies = list(company_info.index)
 
-record_file = Records(records_path / 'Valuation_all.xlsx', initial_tickers=companies, add_tickers=False, online=True)
+record_file = Records(records_path / 'Valuation_all.xlsx', initial_tickers=companies, add_tickers=True, online=True)
 
 #record_file.update_share_counts(intrinsic_online=False)
 #record_file.update()
 with keep.presenting():
-    #record_file.update_prices()
-    #record_file.update_intrinsic_values()
-    #record_file.online = False
-    #record_file.update_intrinsic_values(quarter=True)
+    record_file.update_prices()
+    record_file.update_intrinsic_values()
+    record_file.online = False
+    record_file.update_intrinsic_values(quarter=True)
     record_file.update_revenue_change()
     record_file.update_last_quarter()
 
