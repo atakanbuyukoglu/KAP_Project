b715d86027976a85e9196f715e65b3d1a9be6db9
Atakan Buyukoglu <buyukoglu.atakan@gmail.com>
2024-07-09 17:01:28 +0300

    Add model for IsYatirim to obtain sector information

--- Test.py
+++ Test.py
@@ -1,12 +1,12 @@
 from src.DataFetch.KAP import KAP
-from src.DataFetch.Helpers.DateHandler import DateHandler
+from src.DataFetch.IsYatirim import IsYatirim
 from pathlib import Path
 from datetime import date, timedelta
 
 data_path = Path(__file__).parent / "Data/"
 
-kap = KAP(data_path)
-x = kap.get_share_count('SISE', online=False)
+isyat = IsYatirim()
+x = isyat.getSector('flap')
 print(x)
 
 #kap.save_financials('AKFK')
--- src/DataFetch/IsYatirim.py
+++ src/DataFetch/IsYatirim.py
@@ -1,15 +1,42 @@
 # Request related imports
 from .Helpers.RequestWrapper import Request
+from .Helpers.utils import standardize_ticker
 
 # Miscellaneous imports
 from pathlib import Path
+import json
 
 SLEEP_TIME = 2.01
 
+'''
+Sample URLs:
+
+https://www.isyatirim.com.tr/_layouts/15/Isyatirim.Website/Common/Data.aspx/HisseTekil?hisse=MEGAP&startdate=22-06-2024&enddate=30-06-2024.json
+
+'''
+
+INFO_PATH = Path(__file__).parents[2] / 'Data' / 'SirketBilgileri.json'
+
+# TODO: Implement this
 class IsYatirim:
 
-    def __init__(self, data_path) -> None:
-        self.companies_path = Path(data_path)
+    def __init__(self) -> None:
+        self.info_path = INFO_PATH
         self.r = Request(sleep_time=SLEEP_TIME)
+        with open(INFO_PATH, 'r', encoding='utf-8') as f:
+            self.info = json.load(f)['value']
 
-        self.company_info = None
+    def get_info(self):
+        return self.info
+    
+    def getSector(self, ticker):
+        ticker = standardize_ticker(ticker)
+        sector = [s['AS_ALT_SEKTOR_TANIMI'] for s in self.info if s['Title'] == ticker]
+        if len(sector) > 0:
+            sector = sector[0]
+        else:
+            sector = 'Diğer'
+        return sector
+
+    
+
